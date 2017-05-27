f3cd72d2c6ccc967c220865ab5a621932ea6f613
Sindiso DUBE <sdube@e5r3p2.jnb.42.fr>
2017-05-27 13:22:04 +0200

    ft_retro

--- models/Vector.cpp
+++ models/Vector.cpp
@@ -1,1 +1,47 @@
 #include <Vector.hpp>
+
+Vector2D::Vector2D() {
+    return;
+}
+
+Vector2D::Vector2D(int x, int y) : x(x) y(y) {
+    return;
+}
+
+/*--------------------------------------------------------*/
+Vector2D Vector2D::Vector2D operator+(const Vector2D& b) {
+    return Vector2D(b.x + this->x, b.y + this->y);
+}
+
+Vector2D Vector::Vector2D operator+(const int factor) {
+    return Vector2D(this->x + factor, this->y + factor);
+}
+
+/*--------------------------------------------------------*/
+Vector2D Vector2D::Vector2D operator-(const Vector2D& b) {
+    return Vector2D(b.x - this->x, b.y - this->y);
+}
+
+Vector2D Vector::Vector2D operator-(const int factor) {
+    return Vector2D(this->x - factor, this->y - factor);
+}
+
+/*--------------------------------------------------------*/
+Vector2D Vector2D::Vector2D operator*(const Vector2D& b) {
+    return Vector2D(b.x * this->x, b.y * this->y);
+}
+
+Vector2D Vector::Vector2D operator*(const int factor) {
+    return Vector2D(this->x * factor, this->y * factor);
+}
+
+/*-------------------------------------------------------*/
+Vector2D Vector2D::Vector2D operator/(const Vector2D& b) {
+    return Vector2D(b.x / this->x, b.y / this->y);
+}
+
+Vector2D Vector::Vector2D operator/(const int factor) {
+    return Vector2D(this->x / factor, this->y / factor);
+}
+
+/*--------------------------------------------------------*/
