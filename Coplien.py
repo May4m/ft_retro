a244d9bdaaa10c4c8d31f57dc4ad892580f7f26d
Sindiso DUBE <sdube@e5r3p2.jnb.42.fr>
2017-05-27 14:45:32 +0200

    Coplien

--- Coplien.py
+++ Coplien.py
@@ -1,4 +1,6 @@
 
+import sys
+
 
 header = \
 """
@@ -25,30 +27,38 @@ imp = \
     *this = src;
 }
 
-
 %s::~%s() {
     return ;
 }
 
 %s& %s::operator=(%s const &rhs)
 {
+    // do fields assignment here
     return *this;
 }
 """
 
 
 def make_file(class_name):
-    header_protector = class_name.uppercase();
+    header_protector = class_name.upper() + "_H";
 
-    name_tuple = [header_protector, header_protector] + [class_name]  * 5
+    name_tuple = [header_protector, header_protector] + ([class_name] * 5)
+    name_tuple = map(str, name_tuple)
+    name_tuple = tuple(name_tuple)
     header_ = header % name_tuple
 
-
-    name_tuple = [class_name + ".hpp"] + [class_name] * 8
+    name_tuple = [class_name + ".hpp"] + ([class_name] * 8)
+    name_tuple = map(str, name_tuple)
+    name_tuple = tuple(name_tuple)
     impl_ = imp % name_tuple
 
-    with file(class_name + ".hpp", "w") as hf,
+    with file(class_name + ".hpp", "w") as hf,\
         file(class_name + ".cpp", "w") as mf:
         hf.write(header_)
         mf.write(impl_)
 
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        make_file(sys.argv[1])
